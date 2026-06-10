15084921872188f3ac1684def2d1a247987478b8
Caio <caiopereira552@gmail.com>
2026-06-10 15:49:49 -0300

    format

--- src/components/layout/appBar.vue
+++ src/components/layout/appBar.vue
@@ -3,29 +3,18 @@
     <v-app-bar-nav-icon v-if="isMobile" @click="drawer = !drawer" />
 
     <v-toolbar-title class="pt-1 pb-0">
-      <v-img
-        width="210px"
-        height="210px"
-        src="@/assets/logo-img-w.png"
-        alt="Logo"
-        contain
-      ></v-img>
+      <router-link to="/" class="logo-link">
+        <v-img width="210" height="210" src="@/assets/logo-img-w.png" alt="Logo" contain />
+      </router-link>
     </v-toolbar-title>
 
-    <!-- Desktop menu -->
     <template v-if="!isMobile">
       <v-btn variant="text">Infantil</v-btn>
       <v-btn variant="text">Masculino</v-btn>
       <v-btn variant="text">Feminino</v-btn>
       <v-spacer />
 
-      <v-text-field
-        v-model="search"
-        variant="underlined"
-        label="O que você procura?"
-        density="compact"
-        hide-details
-      />
+      <v-text-field v-model="search" variant="underlined" label="O que você procura?" density="compact" hide-details />
       <v-spacer />
 
       <v-btn variant="text">Login</v-btn>
@@ -34,7 +23,6 @@
     </template>
   </v-app-bar>
 
-  <!-- DRAWER MOBILE -->
   <v-navigation-drawer color="black" v-model="drawer" temporary location="left">
     <v-btn variant="text">Login</v-btn>
     <v-btn variant="text"><v-icon icon="mdi-heart-outline" /></v-btn>
